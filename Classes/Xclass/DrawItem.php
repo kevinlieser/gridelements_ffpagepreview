e7b553992d29db5b2ec940e6e53f7dbe7b090638
Kevin Lieser <info@ka-mediendesign.de>
2018-03-08 14:33:12 +0100

    Multi-Sheet Flexform Support added

--- Classes/Xclass/DrawItem.php
+++ Classes/Xclass/DrawItem.php
@@ -41,40 +41,63 @@ class DrawItem extends \GridElementsTeam\Gridelements\Hooks\DrawItem {
 		$templatePath = \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName('EXT:gridelements_ffpagepreview/Resources/Private/Templates/Table.html');
 		$view = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Fluid\\View\\StandaloneView');
 		$view->setTemplatePathAndFilename($templatePath);
+
+		$layoutSetupData = $layoutSetup['pi_flexform_ds'];
+		if(empty($layoutSetupData) && !empty($layoutSetup['pi_flexform_ds_file'])) {
+			$layoutSetupData = file_get_contents($_SERVER["DOCUMENT_ROOT"].'/'.$layoutSetup['pi_flexform_ds_file']);
+		}
 		
 		// Generate Content
-		if(!empty($row['pi_flexform']) && !empty($layoutSetup['pi_flexform_ds'])) {
+		if(!empty($row['pi_flexform']) && !empty($layoutSetupData)) {
 			$parseXMLValues = \TYPO3\CMS\Core\Utility\GeneralUtility::xml2array($row['pi_flexform']);
-			if($parseXMLValues['data']['sDEF']['lDEF']) {
-				$parseXMLValues = $parseXMLValues['data']['sDEF']['lDEF'];
-				$parseXMLLayout = \TYPO3\CMS\Core\Utility\GeneralUtility::xml2array($layoutSetup['pi_flexform_ds']);
-				if($parseXMLLayout['ROOT']['el']) {
-					$parseXMLLayout = $parseXMLLayout['ROOT']['el'];
-					$previewActive = 0;
-					$dataArray = array();
-					$dataArrayImages = array();
-					foreach($parseXMLValues as $key => $value) {
-						if($parseXMLLayout[$key]['TCEforms']['label'] && !empty($value['vDEF'])) {
-							$previewActive = 1;
-							array_push($dataArray, array('layout' => $parseXMLLayout[$key], 'value' => $value));
-							/*
-							if($parseXMLLayout[$key]['TCEforms']['config']['internal_type'] == 'file') {
-								array_push($dataArrayImages, array('layout' => $parseXMLLayout[$key], 'value' => $value));
-							} else {
-								array_push($dataArray, array('layout' => $parseXMLLayout[$key], 'value' => $value));
+
+			if(count($parseXMLValues['data']) != 0) {
+				foreach($parseXMLValues['data'] as $xmlPage) {
+					if($xmlPage['lDEF']) {
+						$parseXMLValues = $xmlPage['lDEF'];
+						$parseXMLLayout = \TYPO3\CMS\Core\Utility\GeneralUtility::xml2array($layoutSetupData);
+
+						if($parseXMLLayout['ROOT']['el']) {
+							$parseXMLLayout = $parseXMLLayout['ROOT']['el'];
+							$previewActive = 0;
+							$dataArray = array();
+							foreach($parseXMLValues as $key => $value) {
+								if($parseXMLLayout[$key]['TCEforms']['label'] && !empty($value['vDEF'])) {
+									$previewActive = 1;
+									array_push($dataArray, array('layout' => $parseXMLLayout[$key], 'value' => $value));
+								}
 							}
-							*/
+							if($previewActive == 1) {
+								$view->assign('data', $dataArray);
+								$view->assign('imageWidth', $this->imageWidth);
+								$view->assign('imageHeight', $this->imageHeight);
+								$grid .= $view->render();
+							}
+						} else {
+							foreach($parseXMLLayout['sheets'] as $xmlData) {
+								if($xmlData['ROOT']['el']) {
+									$xmlData = $xmlData['ROOT']['el'];
+									$previewActive = 0;
+									$dataArray = array();
+									foreach($parseXMLValues as $key => $value) {
+										if($xmlData[$key]['TCEforms']['label'] && !empty($value['vDEF'])) {
+											$previewActive = 1;
+											array_push($dataArray, array('layout' => $xmlData[$key], 'value' => $value));
+										}
+									}
+									if($previewActive == 1) {
+										$view->assign('data', $dataArray);
+										$view->assign('imageWidth', $this->imageWidth);
+										$view->assign('imageHeight', $this->imageHeight);
+										$grid .= $view->render();
+									}
+								}
+							}
 						}
-					}
-					if($previewActive == 1) {
-						$view->assign('data', $dataArray);
-						$view->assign('dataImages', $dataArrayImages);
-						$view->assign('imageWidth', $this->imageWidth);
-						$view->assign('imageHeight', $this->imageHeight);
-						$grid .= $view->render();
 					}
 				}
 			}
+			
 		}
 		
 		return $grid;
